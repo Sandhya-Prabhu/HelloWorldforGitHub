386c4389d3def3530724c92ab8c9e5ae843c2cd0
Sandhya <sandhya4059@gmail.com>
2016-07-11 12:51:55 +0530

    Added Second Activity

---
+++ app/src/main/java/materialdemo/example/com/helloworldforgithub/SecondActivity.java
@@ -0,0 +1,17 @@
+package materialdemo.example.com.helloworldforgithub;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by SANDHYA on 11-07-2016.
+ */
+public class SecondActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.second);
+    }
+}
